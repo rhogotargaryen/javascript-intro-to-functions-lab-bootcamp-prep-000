a4d084af313e7c435f9f6f51b5df31a385146217
Derek Haller <dhaller.personal@gmail.com>
2017-09-14 21:08:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase) {
-      return "I can\'t hear you!";
+      string = "I can\'t hear you!";
+      return string;
   }
   if (string.toUpperCase === string) {
     string = "YES INDEED!";
